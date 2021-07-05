6cb1a6ec5780f43f9a28120189c4b399831ab9ef
nirajpalkar <nirajp526@gmail.com>
2021-07-05 20:34:32 +0530

    update proj

--- trade_api/settings.py
+++ trade_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sc=oj2a6pxi+iw@i^t*#ugyq3y@+(sjm=g*kcm67)&9#c+v)st'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -122,9 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
